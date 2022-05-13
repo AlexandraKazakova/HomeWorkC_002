e5d24f8bd2ab52d6f5117fc6e6da08c623224ff9
Alexandra <sasha_huliganka@mail.ru>
2022-05-13 22:27:58 +0300

    Ex15.1

--- Exercise15/Program.cs
+++ Exercise15/Program.cs
@@ -1,5 +1,10 @@
 ﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
 Console.WriteLine("Введите число от 1 до 7 -> ");
 int number = int.Parse(Console.ReadLine());
-if(number == 6 || number == 7) Console.WriteLine("Да");
-else Console.WriteLine("Нет");
+
+//if (number == 6 || number == 7) Console.WriteLine("Да");
+//else Console.WriteLine("Нет");
+
+bool weekend = number == 6 || number == 7;
+Console.WriteLine(weekend ? "Да" : "Нет");
