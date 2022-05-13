a8890a58ad4edebb5504614fc446de47416f471d
Alexandra <sasha_huliganka@mail.ru>
2022-05-13 22:40:03 +0300

    Ex10.1

--- Exercise10/Program.cs
+++ Exercise10/Program.cs
@@ -4,3 +4,8 @@ int number = int.Parse(Console.ReadLine());
 int n = number / 10;
 int a = n % 10;
 Console.WriteLine(a);
+
+//Console.WriteLine("Введите трёхзначное число -> ");
+//string number = Console.ReadLine();
+//int lenght = 3;
+//Console.WriteLine(number[1]);
